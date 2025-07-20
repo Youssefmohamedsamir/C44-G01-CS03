b5c7cb2439121116f5764bd45f0980d405789f1e
Youssef <yosefmohmed533255@gmail.com>
2025-07-20 20:27:32 +0300

    Question 2

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -4,7 +4,36 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Q1
+            //int[] Numbers = [1, 2, 3, 4, 5];
+            //int sum = 0;
+            //if(Numbers is not null)
+            //{
+            //    for (int  i = 0; i < Numbers.Length; i++)
+            //    {
+            //        sum += Numbers[i];
+            //    }
+            //    Console.WriteLine($"Sum of Numbers of Array is : {sum} ");
+            //}
+            #endregion
+
+            #region Q2
+            //2.Write a program in C# Sharp to merge two arrays of the same size sorted in ascending order.
+            //int[] Arr01 = [1, 5, 6, 8];
+            //int[] Arr02 = [7, 2, 4, 9];
+
+            //int[] Merage = new int[Arr01.Length + Arr02.Length];
+
+            //Arr01.CopyTo(Merage, 0);
+            //Arr02.CopyTo(Merage, Arr01.Length);
+
+            //Array.Sort(Merage);
+
+            //foreach (var item in Merage)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            #endregion
         }
     }
 }
